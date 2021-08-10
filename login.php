cff352b501f6762694cedba88cd1347d5480023d
Keerthan-A3 <84226500+Keerthan-A3@users.noreply.github.com>
2021-08-10 22:18:18 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+         <nav class="navbar">
+            <img class="logo" src="logo.jpg">
+            <ul>
+                <li><a class="active" href="./index.html">Home</a></li>
+                <li><a href="./login.html">Login</a></li>
+                <li><a href="./sign_up.html">Sign Up</a></li>
+            </ul>
+         </nav>
+	<div class="row">
+                <div class="col-1">
+                    <div class="form-box">
+                        <div class="form">
+                            <form class="login-form" action="" method="post">
+                                <center><h1 class="main-heading">Login Form</h1></center>
+				                <input type="text"placeholder="e-mail"name="e-mail"/>
+				                <input type="password"placeholder="password"name="password"/>
+				                <button>LOGIN</button>
+				            </form>
+	             </div>
+	                </div>
+                </div>
+</div>
+</div>
+</body>
+</html>
